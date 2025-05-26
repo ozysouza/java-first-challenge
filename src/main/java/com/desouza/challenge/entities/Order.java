506ea57d40a129debc81197894cfe0f7113b1826
ozysouza <ozysouzaa@gmail.com>
2025-05-25 23:30:14 -0400

    Create Order file

---
+++ src/main/java/com/desouza/challenge/entities/Order.java
@@ -0,0 +1,42 @@
+package com.desouza.challenge.entities;
+
+public class Order {
+
+    public Integer code;
+    public Double basic;
+    public Double discount;
+
+    public Order() {
+    }
+
+    public Order(Integer code, Double basic, Double discount) {
+        this.code = code;
+        this.basic = basic;
+        this.discount = discount;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Double getBasic() {
+        return basic;
+    }
+
+    public void setBasic(Double basic) {
+        this.basic = basic;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+
+}
